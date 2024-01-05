ccea28ea00c8d6b527eceded20dc94aeb2542eae
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2024-01-05 17:50:28 +0800

    refactor: Improve readability for gameboard module

--- src/script.js
+++ src/script.js
@@ -1,126 +1,21 @@
-// /*
-// ** The Gameboard represents the state of the board
-// */
-
-// const gameboard = (function () {
-//     const positions = 9;
-//     const board = Array(positions).fill(0);
-
-    
-
-//     const getBoard = () => board;
-
-//     const placeMark = (position, token) => {
-//         if (!board[position]) {
-//             board[position] = token;
-//             return { success: true };
-//         } 
-//         console.warn(`Position ${position} is already taken, please choose another one.`);
-//         return { success: false };
-//     }
-
-//     const printBoard = () => {
-//         for (let i = 0; i < positions; i += 3) {
-//             console.log(`${board[i]} ${board[i + 1]} ${board[i + 2]}`)
-//         }
-//     }
-
-//     return { getBoard, placeMark, printBoard };
-// })();
-
-// const gameController = (function (
-//     playerOneName = "Player One", 
-//     playerTwoName = "Player Two"
-//     ) {
-//         const players = [
-//             {
-//                 name: playerOneName,
-//                 token: 1
-//             }, 
-//             {
-//                 name: playerTwoName,
-//                 token: 2
-//             }, 
-//         ]; 
-
-//         let activePlayer = players[0];
-
-//         const switchPlayerTurn = () => {
-//             activePlayer = activePlayer === players[0] ? players[1] : players[0];
-//         };
-
-//         const getActivePlayer = () => activePlayer;
-
-//         const printWhoseTurn = () => {
-//             gameboard.printBoard();
-//             console.log(`${getActivePlayer().name}'s turn.`)
-//         }
-
-//         const checkWinCondition = () => {
-//             const boardArray = gameboard.getBoard();
-//             const winConditionsArray = [
-//                 [0, 1, 2],
-//                 [3, 4, 5],
-//                 [6, 7, 8],
-//                 [0, 3, 6],
-//                 [1, 4, 7],
-//                 [2, 5 ,8],
-//                 [0, 4, 8],
-//                 [2, 4, 6]
-//             ]
-
-//             for (let trio of winConditionsArray) {
-//                 let winningToken;
-//                 console.log(`checking trio: ${trio}`);
-//                 winningToken = (boardArray[trio[0]] == boardArray[trio[1]]) && (boardArray[trio[0]] == boardArray[trio[2]]) ? gameboard.getBoard()[trio[0]] : null;
-                
-//                 if (winningToken) {
-//                     console.log(`Game End. ${winningToken} is the winningToken.`)
-//                     return;
-//                 }
-//             }
-//         }
-
-//         const makeMove = (position) => {
-
-//             console.log(`${getActivePlayer().name} is playing ${getActivePlayer().token} in position ${position}`);
-//             const validMove = gameboard.placeMark(position, getActivePlayer().token);
-
-//             checkWinCondition(); 
-
-//             if (validMove.success) {
-//                 switchPlayerTurn();
-//                 printWhoseTurn();
-//             } else {
-//                 printWhoseTurn();
-//             }
-            
-//         }
-
-//         printWhoseTurn();
-
-//         return { getActivePlayer, makeMove };
-//     })();
-
-/*
-** The Gameboard represents the state of the board
-*/
-
-const gameboard = (function () {
-    const positions = 9;
-    const board = Array(positions).fill(0);    
-
-    const getBoard = () => board;
+// Module for managing gameboard
+const gameboard = (() => {
+    const SIZE = 9;
+    const board = Array(SIZE).fill(null);    
 
     const printBoard = () => {
-        for (let i = 0; i < positions; i += 3) {
-            console.log(`${board[i]} ${board[i + 1]} ${board[i + 2]}`)
+        for (let i = 0; i < SIZE; i += 3) {
+            console.log(`${board[i] || '-'} ${board[i + 1] || '-'} ${board[i + 2] || '-'}`)
         }
     }
 
-    return { getBoard, printBoard };
+    return { 
+        getBoard: () => board, 
+        printBoard 
+    };
 })();
 
+// Module for controlling game logic
 const gameController = (function () {
     const initialisePlayers = () => {
         let playerOneName = prompt("Insert Player 1's Name");
